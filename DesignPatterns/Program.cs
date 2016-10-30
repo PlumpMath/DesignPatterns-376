1508932a863b1bade8edf6ec3324cf3b174e9a2a
chathuranga94 <chathuranga94@live.com>
2016-10-30 08:24:51 +0530

    Singleton

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,25 @@
+﻿using DesignPatterns.Creational;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton S1 = Singleton.Instance;
+            Singleton S2 = Singleton.Instance;
+
+            if(S1 == S2)
+            {
+                Console.WriteLine("Same Instance");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
